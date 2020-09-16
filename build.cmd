9c4222f73441b67f6e8a50b57ab01dc40e8519f6
venkateswari <venkateswari.merla@team.neustar>
2020-09-16 00:58:46 -0400

    add code

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 dotnet restore
 dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1
-dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/csharp_function.zip
-zip -r bin/python_s3_function.zip lambda-functions/s3copy.py
-zip -r bin/python_layer_function.zip lambda-functions/LayerTest.py
-zip -r bin/nodejs_local_function.zip lambda-functions/local-invoke-test.js
+dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package output/csharp_function.zip
+zip -r output/python_s3_function.zip lambda-functions/s3copy.py
+zip -r output/python_layer_function.zip lambda-functions/LayerTest.py
+zip -r output/nodejs_local_function.zip lambda-functions/local-invoke-test.js
